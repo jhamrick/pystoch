b3129978c973dd53640830a06b0a5a7c88aaa855
Jessica B. Hamrick <jhamrick@mit.edu>
2011-04-09 17:28:42 -0400

    Add skeleton files for ERPs and queries

---
+++ src/pystoch/erps.py
@@ -0,0 +1,35 @@
+def binomial(p, n):
+    raise NotImplementedError
+
+def dirichlet(alpha):
+    raise NotImplementedError
+
+def exponential(inv_mean):
+    raise NotImplementedError
+
+def flip(weight=0.5):
+    raise NotImplementedError
+
+def gamma(k, theta):
+    raise NotImplementedError
+
+def gaussian(mean, standard):
+    raise NotImplementedError
+
+def log_flip(weight):
+    raise NotImplementedError
+
+def poisson(lambda):
+    raise NotImplementedError
+
+def sample_discrete(weights):
+    raise NotImplementedError
+
+def sample_index_permutation(size):
+    raise NotImplementedError
+
+def sample_integer(n):
+    raise NotImplementedError
+
+def uniform(a, b):
+    raise NotImplementedError
---
+++ src/pystoch/queries.py
@@ -0,0 +1,5 @@
+def mh_query(samples, lag, defines, query_expr, cond_expr):
+    raise NotImplementedError
+
+def rejection_query(defines, query_expr, cond_expr):
+    raise NotImplementedError
