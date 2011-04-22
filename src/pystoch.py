a50474bf1565d7503ec916a69c63c1dbd89bf0b5
Jessica B. Hamrick <jhamrick@mit.edu>
2011-04-22 15:27:07 -0400

    Remove unnecessary code from pystoch.py

--- src/pystoch.py
+++ src/pystoch.py
@@ -1,19 +1,17 @@
-import ast
 import argparse
+import compile
 
 def parse(program, args):
     source = open(program, 'r').read()
     walk(ast.parse(source))
 
-def walk(node):
-    for n in ast.walk(node):
-        print n
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run a PyStoch program.')
+    parser.add_argument('program', metavar='file', type=str, nargs=1,
+                        help='the PyStoch file to execute')
+    parser.add_argument('arguments', metavar='arg', type=str, nargs='*',
+                        help='arguments to the PyStoch program')
 
-parser = argparse.ArgumentParser(description='Run a PyStoch program.')
-parser.add_argument('program', metavar='file', type=str, nargs=1,
-                    help='the PyStoch file to execute')
-parser.add_argument('arguments', metavar='arg', type=str, nargs='*',
-                    help='arguments to the PyStoch program')
-
-args = parser.parse_args()
-parse(args.program[0], args.arguments)
+    args = parser.parse_args()
+    parse(args.program[0], args.arguments)
+    
