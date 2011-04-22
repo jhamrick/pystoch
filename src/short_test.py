3a781bd73ff05d4ba8efdfbab696ca201989d0d7
Jessica B. Hamrick <jhamrick@mit.edu>
2011-04-22 11:02:43 -0400

    Add short rejection query for testing purposes

---
+++ src/short_test.py
@@ -0,0 +1,27 @@
+from queries import RejectionQuery
+from erps import flip
+import numpy as np
+
+class ShortTest(RejectionQuery):
+
+    def __init__(self):
+        self.a = None
+        self.b = None
+
+    def query_model(self):
+        self.a = flip()
+        if self.a:
+            self.b = flip(0.9)
+        else:
+            self.b = flip(0.4)
+
+    def query(self):
+        return self.a
+
+    def condition(self):
+        return self.b
+
+query = ShortTest()
+samples = [query.sample() for x in range(100)]
+
+print np.mean(samples)
