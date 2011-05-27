f562621fbbf9c11442c1389dfb6e299b27f52a6b
Jessica B. Hamrick <jhamrick@mit.edu>
2011-05-27 12:33:18 -0400

    Poisson tests

---
+++ testing/core/testfiles/poisson1.py
@@ -0,0 +1,22 @@
+import pystoch
+from pystoch import poisson, MetropolisHastings
+
+lam = 1
+
+class Query(MetropolisHastings):
+
+    def __init__(self):
+        pass
+
+    def query_model(self):
+        pass
+
+    def sample(self):
+        global lam
+        return poisson(lam)
+
+    def condition(self):
+        return True
+
+result = Query().run(SAMPLES, LAG)
+exresult = lam
---
+++ testing/core/testfiles/poisson3.py
@@ -0,0 +1,23 @@
+import pystoch
+from pystoch import poisson, MetropolisHastings
+
+TOLERANCE = 0.3
+lam = 10
+
+class Query(MetropolisHastings):
+
+    def __init__(self):
+        pass
+
+    def query_model(self):
+        pass
+
+    def sample(self):
+        global lam
+        return poisson(lam)
+
+    def condition(self):
+        return True
+
+result = Query().run(SAMPLES, LAG)
+exresult = lam
